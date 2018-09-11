eab51a27cdd479f87320a561d9ace89f3d1e7ce6
claudialphonse <claudia.alphonse@coditas.com>
2018-09-11 12:48:23 +0530

    first commit

---
+++ src/app/components/Hello.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './style.css';
+interface IProps{
+    compiler:string,
+    framework:string,
+    bundler:string
+}
+export class Hello extends React.Component<IProps, {}>{
+    render(){
+        return(
+            <h1 className="red">This is a {this.props.framework} application using {this.props.compiler} with {this.props.bundler} </h1>
+        )
+    }
+}
