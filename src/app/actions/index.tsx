54be64cf0443ee28bc948742058a37ae1f946f59
claudialphonse <claudia.alphonse@coditas.com>
2018-11-05 13:34:07 +0530

    push data

---
+++ src/app/actions/index.tsx
@@ -0,0 +1,36 @@
+import { dispatch } from '../util';
+const uuidv1 = require('uuid/v1');
+
+export function addTodo(text: string) {
+  return dispatch({
+    type: 'ADD_TODO',
+    id: uuidv1(),
+    text
+  });
+}
+export function removeTodo(id: number) {
+  return dispatch({
+    type: 'DELETE_TODO',
+    id
+  });
+}
+export function moveDown(id: number, index: number) {
+  return dispatch({
+    type: 'MOVE_DOWN',
+    id,
+    index
+  });
+}
+export function moveUp(id: number, index: number) {
+  return dispatch({
+    type: 'MOVE_UP',
+    id,
+    index
+  });
+}
+export function search(text: string) {
+  return dispatch({
+    type: 'SEARCH_TODO',
+    text
+  });
+}
